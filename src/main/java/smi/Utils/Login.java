c8506eb739125be695baedb410627e57314116a6
banaouasmohamedjilani <banaouas.mohamedjilani@hotmail.com>
2017-03-07 14:26:28 +0100

    07/03/2017 version login success + logout+ session

---
+++ src/main/java/smi/Utils/Login.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smi.Utils;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+
+import smi.Utils.SessionUtils;
+import smi.controller.restconsume.refcontrollerrest;
+import smi.model.securite.Client;
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+	@ManagedProperty(value="#{pwd}")
+	private String pwd;
+	private String msg;
+        @ManagedProperty(value="#{user}")
+	private String user;
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	//validate login
+	public String validateUsernamePassword() {
+		boolean valid = refcontrollerrest.validate(user, pwd);
+		if (valid) {
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", user);
+			return "Acceuil";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(
+					null,
+					new FacesMessage(FacesMessage.SEVERITY_WARN,
+							"Incorrect Username and Passowrd",
+							"Please enter correct username and Password"));
+			return "login";
+		}
+	}
+
+	//logout event, invalidate session
+	public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login.xhtml";
+	}
+}
--- src/main/java/smi/controller/restconsume/refcontrollerrest.java
+++ src/main/java/smi/controller/restconsume/refcontrollerrest.java
@@ -1,44 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package smi.controller.restconsume;
 
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import smi.model.securite.Client;
 
-/**
- *
- * @author Djo-Workstation
- */
 public class refcontrollerrest {
-    private String url = "http://169.254.223.24:7258/STB_Serveur";
-
-
-    
-    public Client getClientLogin(String login,String pwd) {
-        Client client= new Client();
+   public static boolean validate (String login , String pwd)
+{ 
+    try{
         ResteasyClient clients = new ResteasyClientBuilder().build();
-        GenericType<Client> list = new GenericType<Client>() {
-        };
-//        String user = getUser();
-        ResteasyWebTarget target = clients.target(url+"sec/getclientlogin")
-                .queryParam("login", login);
-
-        client = target.request(MediaType.APPLICATION_JSON).get(list);
-//        String password = client.getPassword();
-//        
-//        if ((client!=null)&(password == pwd ))
-//                return client;
-//            
-//            
-
-        return client;
+        ResteasyWebTarget target = clients.target("http://169.254.223.24:7258/STB_Serveur/sec/getclientlogin")
+                .queryParam("login",login);
+       Client c  = target.request(MediaType.APPLICATION_JSON).get(Client.class);
+       System.out.print(c.getClientPK().getTypePiecePersonne()+"  " +c.getPassword());
+    if (c != null )
+    {  System.out.println("Login success "+c.toString());
+        return true;
+   
+    
+    }}
+    catch (Exception ex){
+        
+       System.out.print("login "+login +"and "+pwd);
+        System.out.println("Login error "+ex.getMessage());
+        return false;
     }
+    
+    return false;
+ }
 }
