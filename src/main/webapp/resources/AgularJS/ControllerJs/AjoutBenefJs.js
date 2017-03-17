ea771441880eacc27eeb76cdf4c8f5f38fad19a1
banaouasmohamedjilani <banaouas.mohamedjilani@hotmail.com>
2017-03-17 17:13:44 +0100

    17/03/2017 version finale stable

--- src/main/webapp/resources/AgularJS/ControllerJs/AjoutBenefJs.js
+++ src/main/webapp/resources/AgularJS/ControllerJs/AjoutBenefJs.js
@@ -1,22 +1,59 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", ['datatables']);
 
-myApp.controller("myController", function ($scope) {
+myApp.controller("myController", function ($scope,$http) {
+   $scope.dtOptions = {responsive : true}
     $scope.newUser = {};
     $scope.users = [];
-        console.log($scope.users);
+    $scope.isdisabled= true;
+    $scope.checkVide = function(){
+        console.log($scope.users.length + "222");
+        if ($scope.users.length > 0){
+            $scope.isdisabled=false;
+            return false;
+        }
+        return true;
+        
+    };
     
+   $scope.edit =function(index){
+     console.log($scope.users[index]+"index");
+     $scope.index = index;
+       
+   };
+        console.log($scope.users.length+"111");
+        console.log($scope.users);
+    $http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
+        $scope.objects = response.data;
+      console.log($scope.objects);
+  });
+  $scope.index = null;
     $scope.save = function () {
-        if ($scope.newUser.Typepiece != null)
+        if (($scope.newUser.Typepiece != null) && ($scope.newUser.Numero != null) && ($scope.newUser.DatePiece != null              ) && ($scope.newUser.Nom != null) && ($scope.newUser.Adresse != null) && ($scope.newUser.Qualite != null                ) && ($scope.newUser.Etat != null)){
+            if($scope.index === null){
         $scope.users.push($scope.newUser);
-        $scope.newUser = {};
-        console.log($scope.users);
+        console.log($scope.users.length);
+        
+    }
+            else {
+                $scope.users[$scope.index]= $scope.newUser ;
+            }
+            $scope.index = null ;
     };
     
-     $scope.delete = function (index) {
-         console.log(index);
-        $scope.users.splice(index, 1);
+    
     };
-});
+   
+    $scope.reset= function (){
+        $scope.newUser ={};
+    };
+    
+     
+     $scope.delete = function (index) {
+         console.log($scope.users.length+"333");
+        $scope.users.splice(index, 1);
+        $scope.reset();
+    };
+    });
     
 //    $http.get("http://169.254.223.24:1111/STB_Serveur/ref/allDeviseQ").then(function(response){
 //       
@@ -25,16 +62,15 @@ myApp.controller("myController", function ($scope) {
 //    });
 myApp.controller("myctrl1",function($scope,$http){
         
-        $http.get("http://169.254.223.24:7258/STB_Serveur/ref/allAgence").then(function(response){
+        $http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
         $scope.objects = response.data;
       console.log($scope.objects);
-        
     });
     
 });
 myApp.controller("myctrl2",function($scope,$http){
         
-        $http.get("http://169.254.223.24:7258/STB_Serveur/ref/allDeviseQ").then(function(response){
+        $http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
         $scope.objects = response.data;
       console.log($scope.objects);
         
--- src/main/webapp/resources/AgularJS/ControllerJs/validformfraisvoyage.js
+++ src/main/webapp/resources/AgularJS/ControllerJs/validformfraisvoyage.js
@@ -1,8 +1,16 @@
 var vform = angular.module("myForm", []);
 
-vform.controller('vform', function ($scope) {
+vform.controller('vform', function ($scope,$http) {
 
-
+$http.get("https://jsonplaceholder.typicode.com/users").then(function(response){
+        $scope.objects = response.data;
+      console.log($scope.objects);
+  });
+  $http.get("https://jsonplaceholder.typicode.com/users?id=1").then(function(response){
+        $scope.Obbj = response.data;
+        
+      console.log($scope.Obbj);
+  });
     $scope.checkErr = function (startDate, endDate) {
         console.log("aaaa");
         $scope.errMessage = "";
@@ -11,13 +19,16 @@ vform.controller('vform', function ($scope) {
 
         if (startDate > endDate) {
             $scope.errMessage = "End Date should be greater than start date";
-            angular.element('#startDate').css('background-color', 'pink');
+            angular.element('#endDate').css('background-color', 'pink');
             return false;
         }
         if (startDate < curDate.setDate(curDate.getDate() - 1)) {
             $scope.errMessage = 'Start date should not be before today.';
+            angular.element('#startDate').css('background-color', 'pink');
             return false;
         }
+        angular.element('#startDate').css('background-color', 'lightgreen');
+        angular.element('#endDate').css('background-color', 'lightgreen');
         return true;
     };
 
